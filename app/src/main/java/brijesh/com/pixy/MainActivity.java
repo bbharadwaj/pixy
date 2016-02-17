e577a5fac8b2641b508176b04afc971fa459eb15
brijesh_bharadwaj <brijesh.v.bharadwaj@gmail.com>
2016-02-17 16:05:47 +0530

    Camera works

--- app/src/main/java/brijesh/com/pixy/MainActivity.java
+++ app/src/main/java/brijesh/com/pixy/MainActivity.java
@@ -1,9 +1,12 @@
 package brijesh.com.pixy;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
@@ -18,7 +21,31 @@ import com.parse.ParseUser;
 public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static final int TAKE_PHOTO_REQUEST = 0;
+    public static final int TAKE_VIDEO_REQUEST = 1;
+    public static final int PICK_PHOTO_REQUEST = 2;
+    public static final int PICK_VIDEO_REQUEST = 3;
 
+
+
+
+    protected DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            switch (i){
+                case 0: //take pic
+                    Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePhotoIntent,TAKE_PHOTO_REQUEST);
+                case 1: //take vid
+                    break;
+                case 2: //select pic
+                    break;
+                case 3: //select video
+                    break;
+
+            }
+        }
+    };
 
     SectionsPagerAdapter mSectionsPagerAdapter;
 
@@ -98,16 +125,24 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id == R.id.action_logout) {
-            ParseUser.logOut();
-            navigateToLogin();
-        }
+        switch (id) {
 
-        else if (id == R.id.action_edit_friends){
-            Intent intent = new Intent(this, EditFriendsActivity.class);
-            startActivity(intent);
-        }
+            case R.id.action_logout:
+                ParseUser.logOut();
+                navigateToLogin();
 
+            case R.id.action_edit_friends:
+
+                Intent intent = new Intent(this, EditFriendsActivity.class);
+                startActivity(intent);
+
+            case R.id.action_camera:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setItems(R.array.camera_choices, mDialogListener);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+        }
         return super.onOptionsItemSelected(item);
     }
 
