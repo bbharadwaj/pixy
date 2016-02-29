1da172e49b022caa5f83bedb9d0cba2643ea1479
brijesh_bharadwaj <brijesh.v.bharadwaj@gmail.com>
2016-02-29 15:34:14 +0530

    Complete! Messages disappear:)

--- app/src/main/java/brijesh/com/pixy/InboxFragment.java
+++ app/src/main/java/brijesh/com/pixy/InboxFragment.java
@@ -1,6 +1,8 @@
 package brijesh.com.pixy;
 
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
@@ -8,13 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -58,14 +63,16 @@ public class InboxFragment extends ListFragment{
                         i++;
                     }
 
-                    ArrayAdapter adapter = new ArrayAdapter<String>(
-                            getListView().getContext(),
-                            android.R.layout.simple_list_item_1,
-                            senderNames
-                    );
+                    if (getListView().getAdapter() == null) {
 
-                    setListAdapter(adapter);
-                    getActivity().setProgressBarIndeterminateVisibility(false);
+                        MessageAdapter adapter = new MessageAdapter(getListView().getContext(), mMessages);
+                        setListAdapter(adapter);
+                        getActivity().setProgressBarIndeterminateVisibility(false);
+                    }
+
+                    else{
+                        ((MessageAdapter)getListView().getAdapter()).refill(mMessages);
+                    }
 
 
                 } else {
@@ -84,4 +91,72 @@ public class InboxFragment extends ListFragment{
             }
         });
     }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ParseObject message = mMessages.get(position);
+        String messageType = message.getString(ParseConstants.KEY_FILE_TYPE);
+        ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
+        Uri fileUri = Uri.parse(file.getUrl());
+
+        if (messageType.equals(ParseConstants.TYPE_IMAGE)){
+
+            Intent intent = new Intent(getActivity(), ViewImageActivity.class);
+            intent.setData(fileUri);
+            startActivity(intent);
+
+        }
+
+        else{
+            Intent intent = new Intent(Intent.ACTION_VIEW,fileUri );
+            intent.setDataAndType(fileUri, "video/*");
+            startActivity(intent);
+
+        }
+
+        //delete the message!
+
+        List<String> ids = message.getList(ParseConstants.KEY_RECIPIENT_IDS);
+
+        if (ids.size() == 1){
+            //current user is the last recipient, delete message
+            message.deleteInBackground();
+        }
+
+        else{
+
+            ArrayList<String> idsToRemove = new ArrayList<String>();
+            idsToRemove.add(ParseUser.getCurrentUser().getObjectId());
+            message.removeAll(ParseConstants.KEY_RECIPIENT_IDS, idsToRemove);
+            message.saveInBackground();
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
